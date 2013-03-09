907819d949805dfd4e201faab58fdc2afbaf9326
JavierOrd <0619020114@alu.uma.es>
2013-03-09 18:00:06 +0100

    modificaciones alumno 1

--- GitAux.java
+++ GitAux.java
@@ -15,11 +15,14 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+	system.out.println("modificado por alum2"); // insertado por alum2
     }
     
     // Comentario metodoComunitario
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+	//modificacaciones alumno 2
+	System.out.prinln("Alumno 2 inserta esta sentencia");
     }
 
     public void testGitAux(){
