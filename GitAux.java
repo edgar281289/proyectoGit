fd8b47de0f019e7582e9bc1bfc5f3e8a27b86f9f
edgar281289 <edgar_281289@uma.es>
2013-03-09 17:49:07 +0100

    GitAux.java modificado

--- GitAux.java
+++ GitAux.java
@@ -11,7 +11,6 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
-	System.out.println("modificado por alum 1"); // insertado por alum1
     }
     
     public void metodoAlumno2(){
@@ -21,8 +20,6 @@ public class GitAux {
     // Comentario metodoComunitario
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
-	// modificaciones alumno 1
-	System.out.println( "Alumno 1 inserta esta sentencia");
     }
 
     public void testGitAux(){
